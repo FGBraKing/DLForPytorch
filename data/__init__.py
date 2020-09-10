670b85c92efc61a4e209c12c6ab0fcba4c77742b
fong <nousfong@foxmail.com>
2020-09-10 13:54:07 +0800

    the basic architecture

---
+++ data/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2020, CoolFong. All rights reserved.
+# @Time    : 2020/9/10
+# @Author  : CoolFong
+"""This is the data for project.
+dataloads: dataload.
+transforms: some function to transform
+ """
---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2020, CoolFong. All rights reserved.
+# @Time    : 2020/9/10
+# @Author  : CoolFong
+"""This is the models for project.
+modules: some basic modules.
+networks: the architecture of network
+ """
